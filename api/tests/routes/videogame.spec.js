699141c54bff8fdcc857f74a614790ddf353fb0a
DaroOps <romerosd089@hotmail.com>
2023-10-25 23:37:23 -0500

    <feat> created test for routes, waiting more tests here

--- api/tests/routes/videogame.spec.js
+++ api/tests/routes/videogame.spec.js
@@ -1,21 +1,19 @@
-/* eslint-disable import/no-extraneous-dependencies */
 const { expect } = require('chai');
 const session = require('supertest-session');
 const app = require('../../src/app.js');
-const { Videogame, conn } = require('../../src/db.js');
+const { Videogames, conn } = require('../../src/db.js');
+const { createNullObject } = require('../utils/createNullObject.js');
 
 const agent = session(app);
-const videogame = {
-  name: 'Super Mario Bros',
-};
+const videogame = createNullObject("");
 
 describe('Videogame routes', () => {
   before(() => conn.authenticate()
   .catch((err) => {
     console.error('Unable to connect to the database:', err);
   }));
-  beforeEach(() => Videogame.sync({ force: true })
-    .then(() => Videogame.create(videogame)));
+  beforeEach(() => Videogames.sync({ force: true })
+    .then(() => Videogames.create(videogame)));
   describe('GET /videogames', () => {
     it('should get 200', () =>
       agent.get('/videogames').expect(200)
