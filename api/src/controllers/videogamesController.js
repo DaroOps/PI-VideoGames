bc3f0a6500cabc6ad0d4653fa642c4b50c2761aa
DaroOps <romerosd089@hotmail.com>
2023-10-25 23:33:06 -0500

    <fix> the service now return the correct data and manage catch error in case of fail

--- api/src/controllers/videogamesController.js
+++ api/src/controllers/videogamesController.js
@@ -2,11 +2,9 @@ const {getvideoGames} = require('../services/videogamesService');
 const catchedAsync = require('../utils/catchedAsync');
 
 const videogameControllers = catchedAsync( async (req, res) => {
-
-   const response =  await getvideoGames(req,res);
+   const response = await getvideoGames();
  
    return res.status(200).json(response);
-  
 });
 
 module.exports = videogameControllers;
